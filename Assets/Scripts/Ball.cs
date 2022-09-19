0efca4af92beafd6122455fa1b034edd5dcd6e12
Furkan Elmalı <46634314+furkanelmali@users.noreply.github.com>
2022-09-19 20:38:03 +0300

    1.3
    Added splash and tile

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -2,14 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class Ball : MonoBehaviour
 {
     Rigidbody rb;
+    public TextMeshProUGUI skorText;
+    public int skor;
     public float jumpForce;
+    public GameObject splashPrefab;
     // Start is called before the first frame update
     void Start()
     {
+       skorReset();
        rb = GetComponent<Rigidbody>();
     }
 
@@ -21,14 +26,29 @@ public class Ball : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
-       if(collision.gameObject.tag == "Safe") 
+        GameObject x = Instantiate(splashPrefab, transform.position + new Vector3(0f,-.2f,0f), transform.rotation);
+        x.transform.SetParent(collision.gameObject.transform);
+        rb.AddForce(Vector3.up * jumpForce);
+        string materialName = collision.gameObject.GetComponent<MeshRenderer>().material.name;
+
+        if (collision.gameObject.tag == "Safe") 
         {
-            rb.AddForce(Vector3.up * jumpForce);
-           
+            skor += 10;
+            skorText.text = skor.ToString();
         }
        else if(collision.gameObject.tag == "Danger") 
         {
             SceneManager.LoadScene(0);
         }
+        else if (collision.gameObject.tag == "Finish")
+        {
+            //Sonraki Level
+        }
+    }
+
+    public void skorReset() 
+    {
+        skor = 0;
+        skorText.text = skor.ToString();
     }
 }
